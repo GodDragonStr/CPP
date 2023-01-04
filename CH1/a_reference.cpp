9ac1d5bade2ae730aad09d5bd09903ae17ded37f
Harry <ghr_shenlong@qq.com>
2023-01-04 13:25:14 +0800

    README.md

---
+++ CH1/a_reference.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 5;
+    int & ref = n;
+    ref = 7;
+    cout << n << endl;
+    cout << ref << endl;
+    n = 4;
+    cout << n << endl;
+    cout << ref << endl;
+
+    return (0);
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Learn C++ from scratch
 
 Moximoxi, this is harry, I am going to travel to planet of C++. -2023.1.4
+
+## CH1
+从C到C++
